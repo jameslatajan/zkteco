e249ac5cdbe0cd77ea41675e6963b49dcc218a8f
Jefferson <jefferson.pimentel173@gmail.com>
2025-07-18 00:17:06 +0800

    Fix PHP 8.1 float-to-int deprecation warning

--- src/Lib/Helper/Util.php
+++ src/Lib/Helper/Util.php
@@ -85,10 +85,10 @@ class Util
   {
     $timestamp = strtotime($t);
     $t = (object)[
-      'year' => (int)date('Y', $timestamp),
-      'month' => (int)date('m', $timestamp),
-      'day' => (int)date('d', $timestamp),
-      'hour' => (int)date('H', $timestamp),
+      'year'   => (int)date('Y', $timestamp),
+      'month'  => (int)date('m', $timestamp),
+      'day'    => (int)date('d', $timestamp),
+      'hour'   => (int)date('H', $timestamp),
       'minute' => (int)date('i', $timestamp),
       'second' => (int)date('s', $timestamp),
     ];
@@ -108,29 +108,30 @@ class Util
    */
   static public function decodeTime($t)
   {
+    $t = (int) round($t); // Clean float-to-int conversion upfront
+
     $second = $t % 60;
-    $t = $t / 60;
+    $t = intdiv($t, 60);
 
     $minute = $t % 60;
-    $t = $t / 60;
+    $t = intdiv($t, 60);
 
     $hour = $t % 24;
-    $t = $t / 24;
+    $t = intdiv($t, 24);
 
     $day = $t % 31 + 1;
-    $t = $t / 31;
+    $t = intdiv($t, 31);
 
     $month = $t % 12 + 1;
-    $t = $t / 12;
+    $t = intdiv($t, 12);
 
-    $year = floor($t + 2000);
+    $year = $t + 2000;
 
-    $d = date('Y-m-d H:i:s', strtotime(
-      $year . '-' . $month . '-' . $day . ' ' . $hour . ':' . $minute . ':' . $second
+    return date('Y-m-d H:i:s', strtotime(
+      sprintf('%04d-%02d-%02d %02d:%02d:%02d', $year, $month, $day, $hour, $minute, $second)
     ));
-
-    return $d;
   }
+
 
   /**
    * @param string $hex
@@ -204,7 +205,7 @@ class Util
     }
 
     if ($chksum > 0) {
-      $chksum = -($chksum);
+      $chksum = - ($chksum);
     } else {
       $chksum = abs($chksum);
     }
@@ -245,7 +246,6 @@ class Util
     $buf = pack('SSSS', $command, $chksum, $session_id, $reply_id);
 
     return $buf . $command_string;
-
   }
 
   /**
